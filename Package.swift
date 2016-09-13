601c01bcca4555b333144a7199a5c0300e1cce2c
Han Sangjin <tinysun.net@gmail.com>
2016-09-13 12:19:47 +0900

    Update Package.swift
    Changed the package url for Win32

--- Package.swift
+++ Package.swift
@@ -3,6 +3,6 @@ import PackageDescription
 let package = Package(
         name: "W32App",
         dependencies: [
-            .Package(url: "https://github.com/tinysun212/Win32.git", majorVersion: 1, minor: 0)
+            .Package(url: "https://github.com/tumasgiu/Win32.git", majorVersion: 0, minor: 1)
         ]
 )
