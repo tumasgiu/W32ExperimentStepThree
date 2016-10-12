534d3553ebbf7153a9cc64d226b2cbaacc73236e
Thomas Franceschini <tumasgiu@mac.com>
2016-10-12 10:58:07 +0200

    Fix #4, add missing separator

--- Package.swift
+++ Package.swift
@@ -1,9 +1,9 @@
 import PackageDescription
 
 let package = Package(
-        name: "W32App",
-        dependencies: [
-            .Package(url: "https://github.com/tumasgiu/Win32.git", majorVersion: 0, minor: 2)
-            .Package(url: "https://github.com/tumasgiu/GDI.git", majorVersion: 0, minor: 1)
-        ]
+    name: "W32App",
+    dependencies: [
+        .Package(url: "https://github.com/tumasgiu/Win32.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/tumasgiu/GDI.git", majorVersion: 0, minor: 1)
+    ]
 )
