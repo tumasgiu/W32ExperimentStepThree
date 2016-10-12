49e56dd7e5ec20e98543ed045c900d04fa950413
Thomas Franceschini <tumasgiu@mac.com>
2016-10-12 11:17:36 +0200

    Remove Win32 from dependencies
    This avoids an error from SPM, Win32 package is in GDI dependencies so it'll be fetched anyway.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,6 @@ import PackageDescription
 let package = Package(
     name: "W32App",
     dependencies: [
-        .Package(url: "https://github.com/tumasgiu/Win32.git", majorVersion: 0, minor: 2),
         .Package(url: "https://github.com/tumasgiu/GDI.git", majorVersion: 0, minor: 1)
     ]
 )
